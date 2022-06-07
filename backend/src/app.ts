61efe9704cedb1fa724f479b76d1d99659a746a4
Guilherme Polippo <luispolippo@hotmail.com>
2022-06-06 21:13:01 -0300

    feat: Create App class

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,1 +1,28 @@
-// TODO - App class
+import express, { Router } from 'express';
+
+class App {
+  public app: express.Application;
+
+  constructor() {
+    this.app = express();
+    this.app.use(express.json());
+  }
+
+  public startServer(PORT: string | number = 3001): void {
+    this.app.listen(
+      PORT,
+      // eslint-disable-next-line no-console
+      () => console.log(`Server running here 👉 http://localhost:${PORT}`),
+    );
+  }
+
+  public addRouter(router: Router) {
+    this.app.use(router);
+  }
+
+  public getApp() {
+    return this.app;
+  }
+}
+
+export default App;
