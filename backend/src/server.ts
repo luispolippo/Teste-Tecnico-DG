7a43489cd125f87f5d90a2abac25cd00efd6761e
Guilherme Polippo <luispolippo@hotmail.com>
2022-06-07 01:52:49 -0300

    feat: Instance all User classes

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,5 +1,16 @@
+import { PrismaClient } from '@prisma/client';
 import App from './app';
+import UserController from './controllers/userController';
+import UserModel from './models/userModel';
+import UserRouter from './routes/userRouter';
+import UserService from './services/userService';
 
-const server = new App();
+const prisma = new PrismaClient();
+const userModel = new UserModel(prisma);
+const userService = new UserService(userModel);
+const userController = new UserController(userService);
+const userRouter = new UserRouter(userController);
+
+const server = new App(userRouter);
 
 export default server;
