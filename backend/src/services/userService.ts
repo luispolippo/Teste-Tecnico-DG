c3a70599c6aaa8bd7de96b7d52044aacc7f51e56
Guilherme Polippo <luispolippo@hotmail.com>
2022-06-07 02:09:23 -0300

    feat: Add method getAge to calc users age

--- backend/src/services/userService.ts
+++ backend/src/services/userService.ts
@@ -1,12 +1,28 @@
 import UserModel from '../models/userModel';
-import { User } from '../interfaces/User';
+import { UserWithAge } from '../interfaces/User';
 
 class UserService {
   constructor(private userModel: UserModel) {}
 
-  async getUsers(): Promise<User[]> {
+  static getAge(dateString: Date): number {
+    const today = new Date();
+    const birthDate = new Date(dateString);
+    let age = today.getFullYear() - birthDate.getFullYear();
+    const month = today.getMonth() - birthDate.getMonth();
+    if (month < 0 || (month === 0 && today.getDate() < birthDate.getDate())) {
+      age -= 1;
+    }
+    return age;
+  }
+
+  async getUsers(): Promise<UserWithAge[]> {
     const users = await this.userModel.getUsers();
-    return users;
+    const usersWithAge = users.map((user) => ({
+      id: user.id,
+      name: user.name,
+      age: UserService.getAge(user.birthDate),
+    }));
+    return usersWithAge;
   }
 }
 
