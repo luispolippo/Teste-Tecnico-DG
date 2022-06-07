ce8261fabefbc26f60e371a05b069b185187c096
Guilherme Polippo <luispolippo@hotmail.com>
2022-06-07 01:51:59 -0300

    feat: Create UserRouter

---
+++ backend/src/routes/userRouter.ts
@@ -0,0 +1,17 @@
+import { Router } from 'express';
+import UserController from '../controllers/userController';
+
+class UserRouter {
+  private route: Router;
+
+  constructor(private userController: UserController) {
+    this.route = Router();
+  }
+
+  getRoutes() {
+    this.route.get('/', (req, res) => this.userController.getUsers(req, res));
+    return this.route;
+  }
+}
+
+export default UserRouter;
