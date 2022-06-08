1e0bb0d58c327af4bc93c5a024c3816bfb781003
Guilherme Polippo <luispolippo@hotmail.com>
2022-06-08 13:45:16 -0300

    refact: Move components from App to the new Home page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,9 @@
-import React, { useState } from 'react';
-import Form from './components/Form';
-import Header from './components/Header';
-import Table from './components/Table';
+import React from 'react';
+import Home from './pages/Home';
 
 function App() {
-
-  const [loading, setLoading] = useState<boolean>(false);
-
   return (
-    <>
-      <Header />
-      <Form loadingAtt={{loading, setLoading}} />
-      <Table loadingAtt={{loading, setLoading}}/>
-    </>
+    <Home />
   );
 }
 
