626add020356d423d3e91ab351648b10f44072f7
Guilherme Polippo <luispolippo@hotmail.com>
2022-06-09 14:19:26 -0300

    refact[frontend]: Change directory sctructure from Pages

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
-import Form from '../components/Form';
-import Header from '../components/Header';
-import Table from '../components/Table';
+import Form from '../../components/Form';
+import Header from '../../components/Header';
+import Table from '../../components/Table';
 
 function Home() {
   const [loading, setLoading] = useState<boolean>(false);
